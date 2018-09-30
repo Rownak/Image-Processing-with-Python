acc001fa15af615239e9424d4733f74bfec3fe73
Rownak <rownak.sust@gmail.com>
2018-09-30 16:44:06 -0600

    rotate img

--- openImg.py
+++ openImg.py
@@ -2,3 +2,8 @@ from PIL import Image
 pil_im = Image.open('empire.jpg')
 pil_im_Gray = Image.open('empire.jpg').convert('L')
 pil_im_Gray.save('empire_gray.jpg')
+box = (100,100,200,200)
+region = pil_im_Gray.crop(box)
+region = region.transpose(Image.ROTATE_180)
+pil_im_Gray.paste(region,box)
+pil_im_Gray.save('empire_gray_rotate.jpg')
